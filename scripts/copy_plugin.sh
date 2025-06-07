c4c18244cdd42afb81ab209ce2b4d0e11784e4ed
JouriR <jouri.roosjen@home.nl>
2025-06-07 12:38:19 +0200

    Initial commit

---
+++ scripts/copy_plugin.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cp build/libs/HardcoreSMPPlugin-1.0-SNAPSHOT.jar server/plugins/update/HardcoreSMPPlugin-1.0-SNAPSHOT.jar
---
+++ src/main/java/com/jouriroosjen/hardcoreSMPPlugin/HardcoreSMPPlugin.java
@@ -0,0 +1,17 @@
+package com.jouriroosjen.hardcoreSMPPlugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HardcoreSMPPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
