06525d635741ea06b8e3239436841560e98e9ea1
JouriR <jouri.roosjen@home.nl>
2025-06-08 14:37:11 +0200

    Forgot to update version

--- src/main/java/com/jouriroosjen/hardcoreSMPPlugin/listeners/PlayerDeathListener.java
+++ src/main/java/com/jouriroosjen/hardcoreSMPPlugin/listeners/PlayerDeathListener.java
@@ -23,7 +23,7 @@ import java.util.UUID;
  * Listener class for handling player death events in the server.
  *
  * @author Jouri Roosjen
- * @version 0.1.1
+ * @version 0.2.0
  */
 public class PlayerDeathListener implements Listener {
     private final JavaPlugin plugin;
