e3d034444d8be7362ccd6cd3aad8fd94c2a3abb7
JouriR <jouri.roosjen@home.nl>
2025-06-08 17:28:46 +0200

    Add piggy_bank table to database

--- src/main/resources/migrations/V1__create_players_table.sql
+++ src/main/resources/migrations/V1__create_players_table.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS players
 (
-    uuid       TEXT PRIMARY KEY                  NOT NULL,
-    username   TEXT                              NOT NULL,
-    is_alive   INTEGER DEFAULT 1 CHECK (is_alive IN (0, 1)),
-    created_at TEXT    DEFAULT (datetime('now')) NOT NULL,
-    updated_at TEXT    DEFAULT (datetime('now')) NOT NULL
+    uuid       TEXT PRIMARY KEY                             NOT NULL,
+    username   TEXT                                         NOT NULL,
+    is_alive   INTEGER DEFAULT 1 CHECK (is_alive IN (0, 1)) NOT NULL,
+    created_at TEXT    DEFAULT (datetime('now'))            NOT NULL,
+    updated_at TEXT    DEFAULT (datetime('now'))            NOT NULL
 );
---
+++ src/main/resources/migrations/V3__create_piggy_bank_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS piggy_bank
+(
+    id          INTEGER PRIMARY KEY                           NOT NULL,
+    player_uuid TEXT                                          NOT NULL,
+    amount      INTEGER                                       NOT NULL,
+    is_assist   INTEGER DEFAULT 0 CHECK (is_assist IN (0, 1)) NOT NULL,
+    created_at  TEXT    DEFAULT (datetime('now'))             NOT NULL,
+
+    FOREIGN KEY (player_uuid) REFERENCES players (uuid) ON DELETE CASCADE
+);
