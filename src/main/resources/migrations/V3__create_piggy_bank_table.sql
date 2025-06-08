b42b08e0be2674c07702292900f0283a8e464c20
JouriR <jouri.roosjen@home.nl>
2025-06-08 17:36:59 +0200

    Add is_penalty column to piggy_bank table

--- src/main/resources/migrations/V3__create_piggy_bank_table.sql
+++ src/main/resources/migrations/V3__create_piggy_bank_table.sql
@@ -1,10 +1,11 @@
 CREATE TABLE IF NOT EXISTS piggy_bank
 (
-    id          INTEGER PRIMARY KEY                           NOT NULL,
-    player_uuid TEXT                                          NOT NULL,
-    amount      INTEGER                                       NOT NULL,
-    is_assist   INTEGER DEFAULT 0 CHECK (is_assist IN (0, 1)) NOT NULL,
-    created_at  TEXT    DEFAULT (datetime('now'))             NOT NULL,
+    id          INTEGER PRIMARY KEY                            NOT NULL,
+    player_uuid TEXT                                           NOT NULL,
+    amount      INTEGER                                        NOT NULL,
+    is_assist   INTEGER DEFAULT 0 CHECK (is_assist IN (0, 1))  NOT NULL,
+    is_penalty  INTEGER DEFAULT 0 CHECK (is_penalty IN (0, 1)) NOT NULL,
+    created_at  TEXT    DEFAULT (datetime('now'))              NOT NULL,
 
     FOREIGN KEY (player_uuid) REFERENCES players (uuid) ON DELETE CASCADE
 );
