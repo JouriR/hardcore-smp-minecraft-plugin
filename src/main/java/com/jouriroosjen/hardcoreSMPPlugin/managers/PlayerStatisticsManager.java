310657b9630f0ff5d891653fe88db6addea87604
JouriR <jouri.roosjen@home.nl>
2025-06-17 12:07:27 +0200

    smh

--- src/main/java/com/jouriroosjen/hardcoreSMPPlugin/managers/PlayerStatisticsManager.java
+++ src/main/java/com/jouriroosjen/hardcoreSMPPlugin/managers/PlayerStatisticsManager.java
@@ -8,15 +8,34 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.UUID;
 
+/**
+ * Manages the player statistics.
+ *
+ * @author Jouri Roosjen
+ * @version 1.0.0
+ */
 public class PlayerStatisticsManager {
     private final JavaPlugin plugin;
     private final Connection connection;
 
+    /**
+     * Constructs a new {@code PlayerStatisticsManager} instance.
+     *
+     * @param plugin     The main plugin instance.
+     * @param connection The active database connection.
+     */
     public PlayerStatisticsManager(JavaPlugin plugin, Connection connection) {
         this.plugin = plugin;
         this.connection = connection;
     }
 
+    /**
+     * Increments a player statistic using the given amount.
+     *
+     * @param playerUuid      The UUID of the target player.
+     * @param playerStatistic The statistic to update.
+     * @param incrementValue  The value of which to increment the statistic with.
+     */
     public void incrementStatistic(UUID playerUuid, PlayerStatisticsEnum playerStatistic, int incrementValue) {
         try (PreparedStatement statement = connection.prepareStatement("""
                 INSERT INTO player_statistics (player_uuid, statistic_type, value)
