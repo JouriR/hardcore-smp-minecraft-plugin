780771cafe8b13da19483ad9939e068112400ace
JouriR <jouri.roosjen@home.nl>
2025-06-23 18:03:19 +0200

    Build output file without version

--- scripts/copy_plugin.sh
+++ scripts/copy_plugin.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cp build/libs/HardcoreSMPPlugin-1.0-SNAPSHOT.jar server/plugins/update/HardcoreSMPPlugin-1.0-SNAPSHOT.jar
+cp build/libs/HardcoreSMPPlugin.jar server/plugins/update/HardcoreSMPPlugin.jar
