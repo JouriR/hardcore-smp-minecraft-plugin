e4dce113111c938100a8dc12c81ffe949b4a1458
JouriR <jouri.roosjen@home.nl>
2025-07-03 19:06:51 +0200

    Optimize PlayerAdvancementDoneListener

--- src/main/java/com/jouriroosjen/hardcoreSMPPlugin/listeners/PlayerAdvancementDoneListener.java
+++ src/main/java/com/jouriroosjen/hardcoreSMPPlugin/listeners/PlayerAdvancementDoneListener.java
@@ -2,18 +2,28 @@ package com.jouriroosjen.hardcoreSMPPlugin.listeners;
 
 import com.jouriroosjen.hardcoreSMPPlugin.enums.PlayerStatisticsEnum;
 import com.jouriroosjen.hardcoreSMPPlugin.managers.PlayerStatisticsManager;
+import org.bukkit.NamespacedKey;
+import org.bukkit.advancement.Advancement;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerAdvancementDoneEvent;
+
+import java.util.Set;
 
 /**
  * Handles player advancement done events.
  *
  * @author Jouri Roosjen
- * @version 1.0.0
+ * @version 2.0.0
  */
 public class PlayerAdvancementDoneListener implements Listener {
     private final PlayerStatisticsManager playerStatisticsManager;
+
+    private static final Set<String> TRACKED_ADVANCEMENT_KEYS = Set.of(
+            "story", "nether", "end",
+            "adventure", "husbandry"
+    );
 
     /**
      * Constructs a new {@code PlayerAdvancementDoneListener} instance.
@@ -29,8 +39,17 @@ public class PlayerAdvancementDoneListener implements Listener {
      *
      * @param event The player advancement done event.
      */
-    @EventHandler
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onPlayerAdvancementDone(PlayerAdvancementDoneEvent event) {
+        Advancement advancement = event.getAdvancement();
+        if (advancement == null) return;
+
+        NamespacedKey key = advancement.getKey();
+        if (key == null) return;
+
+        String keyString = key.getKey().split("/")[0];
+        if (!TRACKED_ADVANCEMENT_KEYS.contains(keyString)) return;
+
         playerStatisticsManager.incrementStatistic(event.getPlayer().getUniqueId(), PlayerStatisticsEnum.ADVANCEMENTS_DONE, 1);
     }
 }
